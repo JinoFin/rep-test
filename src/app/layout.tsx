0c9920c9b1782b3701a05da958d7ae8e77862c0e
JinoFin <seifjenane@gmail.com>
2026-06-27 22:46:23 +0200

    remove efficiency showcase from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import DemoActions from "./DemoActions";
-import EfficiencyShowcase from "./EfficiencyShowcase";
 import "./globals.css";
 import "./product-visuals.css";
-import "./efficiency-showcase.css";
 
 export const metadata: Metadata = {
   title: "InstaOps CRM",
@@ -14,7 +12,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <EfficiencyShowcase />
         {children}
         <DemoActions />
       </body>
